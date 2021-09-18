9deadf6e9c25edee4ff6e4eec326d51eead4a6d8
Jose Vega <jocvegar@gmail.com>
2021-09-18 17:15:39 -0600

    able to query stuff :)

--- app/services/test_service.rb
+++ app/services/test_service.rb
@@ -1,9 +1,20 @@
 class TestService < ApplicationService
+  require "google/cloud/bigquery"
+  ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "bigquery-learn-test-860b6e369f13.json"
+
   def initialize(name="")
     @name = name
   end
 
   def call
-    puts "Hola #{@name}!!"
+    bigquery = Google::Cloud::Bigquery.new
+     sql = "SELECT * FROM `bigquery-learn-test.teset_dataset.top_10_tech`" \
+           "LIMIT 100"
+
+     results = bigquery.query(sql)
+
+     results.each do |row|
+       puts row.inspect
+     end
   end
 end
